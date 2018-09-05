febf693daaec21027ad0cedae1d1b8c2fcdce7bc
Jakob Boisen <jakob@fumi.dk>
2018-09-05 13:00:27 +0200

    Initial Release

---
+++ MPlusLeaderBoardTester/ConnectedRealms.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MPlusLeaderBoardTester
+{
+    public class ConnectedRealms
+    {
+        [JsonProperty("_links")]
+        public Links Links { get; set; }
+
+        [JsonProperty("connected_realms")]
+        public Self[] ConnectedRealmsConnectedRealms { get; set; }
+    }
+
+    public class Self
+    {
+        [JsonProperty("href")]
+        public string Href { get; set; }
+    }
+
+    public class Links
+    {
+        [JsonProperty("self")]
+        public Self Self { get; set; }
+    }
+}
---
+++ MPlusLeaderBoardTester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MPlusLeaderBoardTester
+{
+    class Program
+    {
+        public int CurrentPeriod = 662;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.run();
+        }
+        public void run()
+        {
+
+        }
+        public void GetConnectedRealms()
+        {
+
+        }
+    }
+}
